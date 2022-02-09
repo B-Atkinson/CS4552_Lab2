06c2fb28b0b121ef2c9f184934b0330210336d1c
B-Atkinson <73620879+B-Atkinson@users.noreply.github.com>
2022-02-09 14:22:34 -0800

    changes

--- pt1.py
+++ pt1.py
@@ -6,71 +6,67 @@ different controllers, by creating a custom Switch() subclass.
 """
 from mininet.topo import Topo
 from mininet.net import Mininet
-from mininet.node import Controller
+from mininet.node import Controller, OVSSwitch
 from mininet.log import setLogLevel, info
 from mininet.cli import CLI
 from mininet.util import dumpNodeConnections
 
 
-class Lab1(Topo):
-    '''The topology from Lab 1 with switches [A,A2,B,C,D,E,F], two hosts [H1, H2], and an SDN controller connected to switch D.'''
-    def build(self):
-        net = Mininet( topo=None, build=False)
-        
-        # Add hosts
-        leftHost = net.addHost( 'h1' )
-        rightHost = net.addHost( 'h2' )
-        
-        # Add switches
-        switchA = net.addSwitch( 's1' )
-        switchA2 = net.addSwitch( 's2' )
-        switchB = net.addSwitch( 's3' )
-        switchC = net.addSwitch( 's4' )
-        switchD = net.addSwitch( 's5' )
-        switchE = net.addSwitch( 's6' )
-        switchF = net.addSwitch( 's7' )
-        
-        # Add OpenFlow controller
-        c0 = net.addController(name='c0',controller=Controller)
-
-        # Add links
-        net.addLink( leftHost, switchA )
-        net.addLink( switchA, switchA2 )
-        net.addLink( switchA2, switchB )
-        net.addLink( switchB, switchC )
-        net.addLink( switchC, switchD )
-        net.addLink( switchC, switchE )
-        net.addLink( switchE, switchF )
-        net.addLink( switchD, switchF )
-        net.addLink( switchF, rightHost )
-
-        # Start network
-        info( '*** Starting controllers\n')
-        c0.start()
-
-        info( '*** Starting switches\n')
-        net.get('s1').start([c0])
-        net.get('s2').start([c0])
-        net.get('s3').start([c0])
-        net.get('s4').start([c0])
-        net.get('s5').start([c0])
-        net.get('s6').start([c0])
-        net.get('s7').start([c0])
-
-        info( '*** Post configure switches and hosts\n')
-        CLI(net)
-        
-
-
-if __name__ == '__main__': 
-    setLogLevel('info')  
-    net = Mininet(Lab1())
-    net.start()
+def Lab1(self):
+    net = Mininet( switch=OVSSwitch)
     
+    # Add hosts
+    leftHost = net.addHost( 'h1', ip='10.0.2.1' )
+    rightHost = net.addHost( 'h2', ip='10.0.2.2' )
+    
+    # Add switches
+    switchA = net.addSwitch( 's1', cls=OVSSwitch )
+    switchA2 = net.addSwitch( 's2', cls=OVSSwitch )
+    switchB = net.addSwitch( 's3', cls=OVSSwitch )
+    switchC = net.addSwitch( 's4', cls=OVSSwitch )
+    switchD = net.addSwitch( 's5', cls=OVSSwitch )
+    switchE = net.addSwitch( 's6', cls=OVSSwitch )
+    switchF = net.addSwitch( 's7', cls=OVSSwitch )
+    
+    # Add OpenFlow controller
+    c0 = net.addController(name='c0',port=6684)
+    
+    # Add links
+    net.addLink( leftHost, switchA )
+    net.addLink( switchA, switchA2 )
+    net.addLink( switchA2, switchB )
+    net.addLink( switchB, switchC )
+    net.addLink( switchC, switchD )
+    net.addLink( switchC, switchE )
+    net.addLink( switchE, switchF )
+    net.addLink( switchD, switchF )
+    net.addLink( switchF, rightHost )
+    
+    # Start network
+    info( '*** Starting controllers\n')
+    switchD.start([c0])
+    
+    info( '*** Starting switches\n')
+    switchA.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchA2.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchB.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchC.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchD.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchE.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    switchF.cmd('ovs-vsct1 set bridge s1 stp-enable=true')
+    
+    info( '*** Post configure switches and hosts\n')
+    CLI(net)
+        
+
     print('\n\nHost connections:')
     dumpNodeConnections(net.hosts)
     
     print( '\n\nTesting network connectivity' )
     net.pingAll()
     net.stop()
+    
+if __name__ == '__main__': 
+    setLogLevel('info') 
+    Lab1()
     
